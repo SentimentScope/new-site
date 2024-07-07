e2896c5735d4762b67d75bf32f7b75330fbf1ede
SentimentScope <freshgaltdocs@gmail.com>
2024-07-07 18:16:02 +0700

    sdiufhas

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/concepts', title: 'Concepts' },
   { href: '/about', title: 'About' },
 ]
 
